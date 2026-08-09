52ba945dc7e353ec06f0de77aadbbbfce6a15a0d
mohamed_bnz <simobnz81@gmail.com>
2026-08-09 19:29:55 +0100

    ajouter les file route et les page home et login et aussi notfound dans  React frontend

--- Front-End/src/App.jsx
+++ Front-End/src/App.jsx
@@ -1,14 +1,11 @@
-import { useState } from 'react'
 
-import './App.css'
+import { RouterProvider } from "react-router-dom";
+import { router } from "./routes/route";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      
-      
+       <RouterProvider router={router} />
     </>
   )
 }
