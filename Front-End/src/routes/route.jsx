0ff139ada8279668fff0d9172706d5648d356a5a
mohamed_bnz <simobnz81@gmail.com>
2026-08-11 09:44:00 +0100

    ajouter le code du formulaire de page login

--- Front-End/src/routes/route.jsx
+++ Front-End/src/routes/route.jsx
@@ -1,21 +1,30 @@
 import { createBrowserRouter } from "react-router-dom";
+import Sidebar from "../layouts/Sidebar";
 import { Home } from "../components/Home";
 import { Login } from "../components/Login";
 import { NotFound } from "../components/NotFound";
+import { AdminDash } from "../components/Admin/AdminDash";
 
-export const router = createBrowserRouter(
-    [ 
-    {
-        path:'/',
-        element:<Home />
-    },
-    {
-        path:'/login',
-        element:<Login />
-    },
-    {
-        path:'/*',
-        element:<NotFound />
-    },
-]
-)
+export const router = createBrowserRouter([
+  {
+    element: <Sidebar />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/adminDashboard",
+        element: <AdminDash />,
+      },
+      {
+        path: "*",
+        element: <NotFound />,
+      },
+    ],
+  },
+]);
